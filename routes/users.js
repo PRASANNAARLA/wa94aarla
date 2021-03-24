d78016286332b9c6f494ae0546bf814a55b716dd
PRASANNAARLA <S542294@nwmissouri.edu>
2021-03-24 17:47:36 -0500

    users file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var arla=0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  arla=arla+1;
+  res.send('“User accesses are: '+arla);
 });
 
 module.exports = router;
